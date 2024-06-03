35be6c1cb86bf9ee24eea8f58a6c862ac9ef23dc
Denise Deng <siktang1221@gmail.com>
2024-06-03 15:34:22 -0400

    Updated Navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import './Navbar.scss'
+import avatar from '../../assets/images/Mohan-muruge.jpg'
+
+export default function Navbar() {
+    return (
+        <div className='navbar'>
+            <div className='navbar__search'>
+                <div>
+                    <label htmlFor='searchBox'></label>
+                    <input type='search' id='searchBox' name='searchBox' placeholder='Search' className='navbar__search--searchBox'/>
+                </div>
+                <img src={avatar} alt='Avatar-for-mobile' className='navbar__avatar--mobile' />
+            </div>
+            <button type='button' className='navbar__button'>
+                UPLOAD
+            </button>
+            <img src={avatar} alt='Avatar-for-tablet' className='navbar__avatar--tablet' />
+        </div>
+    )
+}
