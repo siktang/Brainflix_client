21626ecca43803204a3109ba89a64b397b459258
Denise Deng <siktang1221@gmail.com>
2024-06-06 16:22:02 -0400

    Updated Main component with VideoInfo

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import CurrentVideo from "../CurrentVideo/CurrentVideo";
+import VideoInfo from "../VideoInfo/VideoInfo";
 import Form from "../Form/Form";
 
 export default function Main( {currentVideo, time} ) {
     return (
-        <>
-            <CurrentVideo currentVideo={currentVideo} time={time}/>
+        <main>
+            <CurrentVideo currentVideo={currentVideo} time={time} />
+            <VideoInfo currentVideo={currentVideo} time={time} />
             <Form />
-        </>
+        </main>
     )
 }
---
+++ src/components/VideoInfo/VideoInfo.jsx
@@ -0,0 +1,31 @@
+import viewIcon from '../../assets/images/icons/views.svg'
+import likeIcon from '../../assets/images/icons/likes.svg'
+
+export default function VideoInfo({ currentVideo, time }) {
+    return (
+        <>
+            <div className='videoInfo'>
+                <h1 className='videoInfo__title'>{currentVideo.title}</h1>
+                <div className='videoInfo__divider'></div>
+                <div className='videoInfo__container'>
+                    <div className='videoInfo__details'>
+                        <h2 className='videoInfo__details--channel'>By {currentVideo.channel}</h2>
+                        <h3 className='videoInfo__details--time'>{time.toLocaleDateString('es-pa')}</h3>
+                    </div>
+                    <div className='videoInfo__stat'>
+                        <div className='videoInfo__stat-boxForViews'>
+                            <img src={viewIcon} alt='icon-for-views' />
+                            <h3 className='videoInfo__stat--views'>{currentVideo.views}</h3>
+                        </div>
+                        <div className='videoInfo__stat-boxForLikes'>
+                            <img src={likeIcon} alt='icon-for-likes' />
+                            <h3 className='videoInfo__stat--likes'>{currentVideo.likes}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='divider'></div>
+            <div className='videoDesc'>{currentVideo.description}</div>
+        </>
+    )
+}
