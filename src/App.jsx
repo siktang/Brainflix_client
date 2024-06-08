69f8d501dc6b85c955be16d600e852e27b7a5b8a
Denise Deng <siktang1221@gmail.com>
2024-06-07 22:37:56 -0400

    Updated styling for all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,15 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.scss'
 import Header from './components/Header/Header'
-import videoList from '../src/data/video-details.json'
-import Main from './components/Main/Main'
+// import videoList from '../src/data/video-details.json'
+import MainPage from './components/MainPage/MainPage'
 
 function App() {
-  const [currentVideo, setCurrentVideo] = useState(videoList[0]);
-
-  const time = new Date(currentVideo.timestamp);
-
-  const nextVideos = videoList.filter((video) => (
-    video.id !== currentVideo.id
-  ) );
-
-  const changeCurrentVideo = (videoId) => {
-    const selectedVideo = videoList.find((video) => (
-      video.id === videoId
-    ));
-    setCurrentVideo(selectedVideo);
-  }
-
+  
   return (
     <>
       <Header />
-      <Main currentVideo={currentVideo} time={time} nextVideos={nextVideos} changeCurrentVideo={changeCurrentVideo}/>
+      <MainPage />
     </>
   )
 }
---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import CurrentVideo from "../CurrentVideo/CurrentVideo";
+import VideoInfo from "../VideoInfo/VideoInfo";
+import Form from "../Form/Form";
+import DisplayComments from "../DisplayComments/DisplayComments";
+import SideBar from "../SideBar/SideBar";
+import videoList from "../../data/video-details.json";
+import "./MainPage.scss"
+
+export default function Main() {
+    const [currentVideo, setCurrentVideo] = useState(videoList[0]);
+
+    const time = new Date(currentVideo.timestamp);
+
+    const nextVideos = videoList.filter((video) => (
+        video.id !== currentVideo.id
+    ) );
+
+    const changeCurrentVideo = (videoId) => {
+        const selectedVideo = videoList.find((video) => (
+        video.id === videoId
+        ));
+        setCurrentVideo(selectedVideo);
+    }
+    
+    return (
+        <main>
+            <CurrentVideo currentVideo={currentVideo} />
+            <div className="videoInfoContainer">
+                <div className="videoInfoContainer__details">
+                    <VideoInfo currentVideo={currentVideo} time={time} />
+                    <Form />
+                    <DisplayComments currentVideo={currentVideo} />
+                </div>
+                <SideBar nextVideos={nextVideos} changeCurrentVideo={changeCurrentVideo}/>
+            </div>
+        </main>
+    )
+}
