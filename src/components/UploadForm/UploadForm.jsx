c7c2e6d548647f38ebcf77a5a25272bf782c97d7
Denise Deng <siktang1221@gmail.com>
2024-06-10 16:43:44 -0400

    Updated upload page styling for mobile

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -6,12 +6,12 @@ export default function UploadForm() {
             <div className="form__fields">
                 <label htmlFor="title" className="section-header">TITLE YOUR VIDEO</label>
                 <input type="text" name="title" id="title" placeholder="Add a title to your video" className="form__fields--input"/>
-                <label htmlFor="description" className="section-header">TITLE YOUR VIDEO</label>
+                <label htmlFor="description" className="section-header">ADD A VIDEO DESCRIPTION</label>
                 <textarea type="text" name="description" id="description" placeholder="Add a description to your video" className="form__fields--textarea"></textarea>
             </div>
             <div className="form__buttons">
-                <button className="form__buttons--cancel">CANCEL</button>
                 <button className="form__buttons--publish">PUBLISH</button>
+                <button className="form__buttons--cancel">CANCEL</button>            
             </div>
         </form>
     )
--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -4,16 +4,18 @@ import videoThumbnail from "../../assets/images/Upload-video-preview.jpg"
 
 export default function UploadPage() {
     return (
-        <>
+        <main>
             <div className="divider"></div>
-            <h1>Upload Video</h1>
-            <div className="uploadDetails">
-                <div className="uploadDetails__thumbnail">
-                    <h3 className="uploadDetails__thumbnail--title section-header">VIDEO THUMBNAIL</h3>
-                    <img src={videoThumbnail} alt="video thumbnail" className="uploadDetails__thumbnail--image"/>
+            <div className="uploadPageContainer">
+                <h1 className="page-header">Upload Video</h1>
+                <div className="uploadDetails">
+                    <div className="uploadDetails__thumbnail">
+                        <h3 className="uploadDetails__thumbnail--title section-header">VIDEO THUMBNAIL</h3>
+                        <img src={videoThumbnail} alt="video thumbnail" className="uploadDetails__thumbnail--image"/>
+                    </div>
+                    <UploadForm />
                 </div>
-                <UploadForm />
             </div>
-        </>
+        </main>
     )
 }
