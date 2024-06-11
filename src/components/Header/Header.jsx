f4968e666795ce1bc2484013faef8b32e8724f01
Denise Deng <siktang1221@gmail.com>
2024-06-10 22:46:45 -0400

    updated logo with Link to homepage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
-import "./Header.scss"
-import Navbar from "../Navbar/Navbar"
-import logo from "../../assets/images/logo/BrainFlix-logo.svg"
+import { Link } from "react-router-dom";
+import "./Header.scss";
+import Navbar from "../Navbar/Navbar";
+import logo from "../../assets/images/logo/BrainFlix-logo.svg";
 
 
 
 export default function Header() {
     return (
         <header className="header">
-            <img src={logo} alt="Logo" className="logo"/>
+            <Link to="/"><img src={logo} alt="Logo" className="logo"/></Link>
             <Navbar />
         </header>
     );
