2327fa2480a2cfe7733c8f2fd96c3d7e49f2ce4a
Denise Deng <siktang1221@gmail.com>
2024-06-18 22:44:21 -0400

    Updated upload form posting

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -12,12 +12,6 @@ export default function UploadForm() {
 
     const navigate = useNavigate();
 
-    const handleClickPublish = (event) => {
-        event.preventDefault();
-        setModalContent("Upload Successful! You will now be taken to Home Page.");
-        setIsModalOpen(true);
-    }
-
     const handleClickCancel = (event) => {
         event.preventDefault();
         setModalContent("Upload Cancelled. You will now be taken to Home Page.");
@@ -28,40 +22,52 @@ export default function UploadForm() {
         navigate("/");
     }
 
-    const handleFormSubmission = async (event) => {
+    const handleClickPublish = async (event) => {
+        event.preventDefault();
+        console.log("You have successfully uploaded!");
+
         const newVideo = {
             title: event.target.title.value,
             description: event.target.description.value,
+            image: `${API_URL}/images/Upload-video-preview.jpg`
         };
 
-        await axios.post(`${API_URL}/videos`, newVideo);
+        try{
+            await axios.post(`${API_URL}/videos`, newVideo);
+
+            setModalContent("Upload Successful! You will now be taken to Home Page.");
+            setIsModalOpen(true);
+
+            form.reset();
+        }
+        catch(e){console.error(e)};
     }
 
     return (
         <div className="uploadFormContainer">
-        <form className="form" onSubmit={handleFormSubmission}>
-            <div className="form__fields">
-                <label htmlFor="title" className="section-header form__fields--label">TITLE YOUR VIDEO</label>
-                <input type="text" name="title" id="title" placeholder="Add a title to your video" className="form__fields--input"/>
-                <label htmlFor="description" className="section-header form__fields--label">ADD A VIDEO DESCRIPTION</label>
-                <textarea type="text" name="description" id="description" placeholder="Add a description to your video" className="form__fields--textarea"></textarea>
-            </div>
-            <div className="form__buttons">
-                <button className="form__buttons--publish" type="submit" onClick={handleClickPublish}>PUBLISH</button>
-                <button className="form__buttons--cancel" onClick={handleClickCancel}>CANCEL</button>            
-            </div>
-        </form>
-        <Modal
-            isOpen={isModalOepn}
-            contentLabel={modalContent}
-            className="modal"
-            overlayClassName="modal__overlay"
-        >
-            <div>
-                <p className="modal__content">{modalContent}</p>
-                <button onClick={handleCloseModal} className="modal__button">CLOSE</button>
-            </div>
-        </Modal>
+            <form className="form" id="form" onSubmit={handleClickPublish}>
+                <div className="form__fields">
+                    <label htmlFor="title" className="section-header form__fields--label">TITLE YOUR VIDEO</label>
+                    <input type="text" name="title" id="title" placeholder="Add a title to your video" className="form__fields--input"/>
+                    <label htmlFor="description" className="section-header form__fields--label">ADD A VIDEO DESCRIPTION</label>
+                    <textarea type="text" name="description" id="description" placeholder="Add a description to your video" className="form__fields--textarea"></textarea>
+                </div>
+                <div className="form__buttons">
+                    <button className="form__buttons--publish" type="submit">PUBLISH</button>
+                    <button className="form__buttons--cancel" onClick={handleClickCancel}>CANCEL</button>            
+                </div>
+            </form>
+            <Modal
+                isOpen={isModalOepn}
+                contentLabel={modalContent}
+                className="modal"
+                overlayClassName="modal__overlay"
+            >
+                <div>
+                    <p className="modal__content">{modalContent}</p>
+                    <button onClick={handleCloseModal} className="modal__button">CLOSE</button>
+                </div>
+            </Modal>
         </div>
     )
 }
